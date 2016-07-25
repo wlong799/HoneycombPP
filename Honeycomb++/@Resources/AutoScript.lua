40c43ff7e01abd7bcfab8dd41f10070e56828ab1
Will Long <wlong799@gmail.com>
2016-07-25 00:43:28 -0400

    Restructured for possibility of different themes
    Moved all icons into the same folder, and moved all items related to
    colorscheme (i.e. wave background, blue icons) into a folder named Wave
    in the Resources directory. This will make it easier to create new
    themes in the future, and change what is loaded just by specifying a
    different directory. Also cleaned up the lua config script.

--- Honeycomb++/@Resources/AutoScript.lua
+++ Honeycomb++/@Resources/AutoScript.lua
@@ -3,25 +3,19 @@ function Initialize()
     if myname == "NONE" then
         myname = SKIN:GetVariable("CURRENTCONFIG")
     end
-    myname = string.sub(myname, 13, -1)
-    mylocation = "#" .. myname
-    mylocation = mylocation .. "Location#"
-    mylocation = SKIN:ReplaceVariables(mylocation)
-    myprefix = string.lower(myname)
+
+    myname = string.sub(myname, 18, -1)
+    mylocation = SKIN:ReplaceVariables("#" .. myname .. "Location#")
+    theme = SKIN:GetVariable("CurrentTheme")
     
-    banga = "!SetVariable Location \"" .. mylocation
-    banga = banga .. "\""
-    bangb = "!SetOption Icon ImageName \"#@#Images\\" .. myprefix
-    bangb = bangb .. "-icon\""
-    bangc = "!SetOption Wallpaper ImageName \"#@#Images\\" .. myprefix
-    bangc = bangc .. "-bg\""
+    banga = "!SetVariable Location \"" .. mylocation .. "\""
+    bangb = "!SetOption Icon ImageName \"#@#" .. theme .. "\\" .. myname .. "\""
+    bangc = "!SetOption Wallpaper ImageName \"#@#Backgrounds\\" .. myname .. "\""
     
     if SKIN:GetMeter("Icon") ~= nil then
         SKIN:Bang(banga)
         SKIN:Bang(bangb)
     else
-        print(bangc)
         SKIN:Bang(bangc)
     end
-    SKIN:Bang("!Update")
 end
